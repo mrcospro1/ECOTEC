8335f32bb0d5f349566069c70bd2e810a34c332c
Marcos David Benítez <136765748+mrcospro1@users.noreply.github.com>
2025-09-24 22:33:24 +0000

    consulta con mail

--- routes/consulta.routes.js
+++ routes/consulta.routes.js
@@ -3,9 +3,10 @@ const prisma = new PrismaClient();
 const express = require('express');
 const router = express.Router();
 const cors = require('cors');
+const nodemailer=require("nodemailer");
 
 router.use(cors({
-  origin: "https://ecotec-liart.vercel.app"
+  origin: process.env.CORS_ORIGIN
 }));
 
 router.post("/registro", async (req, res) => {
@@ -22,11 +23,24 @@ router.post("/registro", async (req, res) => {
         mail,
       },
     });
-
+  const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER, 
+    pass: process.env.EMAIL_PASS, 
+  },
+  });
+  await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: "marcosbenitez1237@gmail.com",
+      subject: "Nueva consulta",
+      text: `Nombre: ${nombre} ${apellido} - Email: ${mail} - Asunto: ${asunto}`,
+    });
     res.status(201).json({
       mensaje: "Registro exitoso",
       consulta: nuevaConsulta,
     });
+
   } catch (error) {
     console.error("Error al crear consulta:", error);
     res.status(500).json({ error: "Error interno del servidor" });

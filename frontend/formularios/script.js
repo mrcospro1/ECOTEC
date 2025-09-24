710bd31cdd54fe6ddc8ac5c4a8dafc90ff102a81
Juli399 <julianlicera56@gmail.com>
2025-09-24 20:20:30 -0300

    Add files via upload

---
+++ frontend/formularios/script.js
@@ -0,0 +1,25 @@
+const formulario=document.getElementById("registroConsulta");
+formulario.addEventListener("submit", async (e) => {
+      e.preventDefault();
+
+      const data = {
+        nombre: document.getElementById("nombre").value,
+        apellido: document.getElementById("apellido").value,
+        mail: document.getElementById("mail").value,
+        asunto: document.getElementById("asunto").value,
+      };
+      
+      const host =process.env.HOST;
+      const ruta ="/consulta/registro";
+      const res = await fetch(`${host}${ruta}`,{
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data)
+      });
+
+      const result = await res.json();
+      console.log(result);
+      alert(result.mensaje || "Registro completado");
+
+      formulario.reset();
+    });

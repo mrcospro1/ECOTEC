51b7e1a71266734e18250f2603b2609f539de5e6
Marcos David Benítez <136765748+mrcospro1@users.noreply.github.com>
2025-09-26 23:53:22 +0000

    cors 2

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "https://ecotec-liart.vercel.app",
+  origin: CORS_ORIGIN,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));

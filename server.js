0f1259c351c5656b687e6984383ba017b2fc3807
Marcos David Benítez <136765748+mrcospro1@users.noreply.github.com>
2025-09-26 23:56:15 +0000

    cors 3

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: CORS_ORIGIN,
+  origin: process.env.CORS_ORIGIN,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));

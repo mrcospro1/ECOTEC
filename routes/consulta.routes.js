e6688b8accd83d180521492878aa553bb0d8dafc
marcos <marcosbenitez127@gmail.com>
2025-10-07 20:26:47 -0300

    redend api

--- routes/consulta.routes.js
+++ routes/consulta.routes.js
@@ -3,7 +3,9 @@ const prisma = new PrismaClient();
 const express = require('express');
 const router = express.Router();
 const cors = require('cors');
-const nodemailer = require("nodemailer");
+const { Resend } = require('resend');
+
+const resend = new Resend(process.env.RESEND_KEY_API);
 
 router.use(cors({
   origin: process.env.CORS_ORIGIN
@@ -19,27 +21,21 @@ router.post("/registro", async (req, res) => {
     const nuevaConsulta = await prisma.consulta.create({
       data: { nombre, apellido, asunto, mail },
     });
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER, 
-        pass: process.env.EMAIL_PASS, 
-      },
-    });
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to: "marcosbenitez1237@gmail.com",
-      subject: `📩 Nueva consulta`,
-      text: `Consulta #${nuevaConsulta.id} de ${nuevaConsulta.nombre} ${nuevaConsulta.apellido} - Email: ${nuevaConsulta.mail} - Asunto: ${nuevaConsulta.asunto} - Fecha: ${nuevaConsulta.createdAt}`,
+
+    // Envío del correo con Resend
+    const response = await resend.emails.send({
+      from: 'Formulario Web <onboarding@resend.dev>', 
+      to: process.env.EMAIL_USER,
+      subject: '📩 Nueva consulta',
       html: `
         <div style="font-family: Arial, sans-serif; max-width: 600px; margin: auto; border: 1px solid #ddd; border-radius: 8px; overflow: hidden; box-shadow: 0 0 10px rgba(0,0,0,0.1);">
           <div style="background-color: #0952eeff; color: white; padding: 20px; text-align: center;">
             <h1 style="margin: 0; font-size: 24px;">Nueva Consulta Recibida</h1>
           </div>
           <div style="padding: 20px; color: #333;">
-            <p><strong>Cliente N°:</strong> ${nuevaConsulta.id}.</p>
-            <p><strong>Fecha:</strong> ${new Date(nuevaConsulta.fecha).toLocaleString()}.</p>
-            <p><strong>Nombre:</strong> ${nuevaConsulta.nombre} ${nuevaConsulta.apellido}.</p>
+            <p><strong>Cliente N°:</strong> ${nuevaConsulta.id}</p>
+            <p><strong>Fecha:</strong> ${new Date(nuevaConsulta.createdAt).toLocaleString()}</p>
+            <p><strong>Nombre:</strong> ${nuevaConsulta.nombre} ${nuevaConsulta.apellido}</p>
             <p><strong>Email:</strong> ${nuevaConsulta.mail}</p>
             <p><strong>Asunto:</strong></p>
             <div style="background-color: #f7f7f7; padding: 10px; border-radius: 5px; border: 1px solid #eee;">
@@ -52,6 +48,12 @@ router.post("/registro", async (req, res) => {
         </div>
       `,
     });
+
+    if (response.error) {
+      console.error('Error al enviar el correo:', response.error);
+      return res.status(500).json({ error: "Error al enviar el correo" });
+    }
+
     res.status(201).json({
       mensaje: "Registro exitoso",
       consulta: nuevaConsulta,

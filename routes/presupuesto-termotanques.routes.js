8c0941ed0e063102bf53bb91ba2cd35abd10b02e
marcos <marcosbenitez127@gmail.com>
2025-11-11 16:23:03 -0300

    presupuesto 1

--- routes/presupuesto-termotanques.routes.js
+++ routes/presupuesto-termotanques.routes.js
@@ -10,6 +10,13 @@ router.use(cors({
 router.post("/calculo", async (req, res) =>{
 
  const { personas, agua, automatizado, altura } = req.body;
+
+if(!personas || !agua || !automatizado || !altura){
+    return res.status(400).json({ error: "Faltan datos obligatorios para el calculo" });
+}
+const nuevaConsulta = await prisma.Presupuesto-termotanques.create({
+    data: { personas, agua, automatizado, altura },
+});
  console.log(personas,agua,automatizado, altura);
 }
 );
